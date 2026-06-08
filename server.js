a13a77155bd961c9d070eb485632d02b141e126b
milkiees6faceit <milkiees6faceit@gmail.com>
2026-06-08 18:41:31 +0300

    Use custom image for 404 page

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const types = {
   ".html": "text/html; charset=utf-8",
   ".css": "text/css; charset=utf-8",
   ".js": "text/javascript; charset=utf-8",
+  ".jpg": "image/jpeg",
+  ".jpeg": "image/jpeg",
   ".json": "application/json; charset=utf-8",
   ".png": "image/png",
   ".txt": "text/plain; charset=utf-8",
